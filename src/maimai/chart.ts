8abe25cb00078e112e02f804bcc7ee4dbe0f9985
saltcute <me@mizuno.ai>
2026-06-14 18:31:10 -0400

    feat: maimai charts have a explicit type

--- src/maimai/chart.ts
+++ src/maimai/chart.ts
@@ -1,6 +1,15 @@
 import type { BaseChart } from "@base/chart";
 import type { Difficulty } from "./difficulty";
 
+export enum Type {
+    STANDARD = "standard",
+    DELUXE = "deluxe",
+}
+
 export interface Chart extends BaseChart {
+    /**
+     * Type of this chart.
+     */
+    type: Type;
     difficulty: Difficulty;
 }
