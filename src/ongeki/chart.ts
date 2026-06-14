2fe04ee3784f962c1260441ddb3c0848683da7f7
saltcute <me@mizuno.ai>
2026-06-14 19:18:22 -0400

    feat: ongeki charts should have a boss

--- src/ongeki/chart.ts
+++ src/ongeki/chart.ts
@@ -1,6 +1,28 @@
 import type { BaseChart } from "@base/chart";
 import type { Difficulty } from "./difficulty";
 
+export enum LunaticType {
+    NONE = "none",
+    REMASTER = "remaster",
+    LUNATIC = "lunatic",
+}
+
 export interface Chart extends BaseChart {
     difficulty: Difficulty;
+    lunatic: LunaticType;
+    boss: {
+        character: {
+            rarity: string;
+            name: string;
+            /**
+             * Characters with `N` rarity don't have a comment.
+             */
+            comment?: string;
+            /**
+             * An identifier to fetch the card image from the database.
+             */
+            card: string;
+        };
+        level: number;
+    };
 }
--- src/ongeki/database.ts
+++ src/ongeki/database.ts
@@ -1,4 +1,13 @@
 import type { Database as BaseDatabase } from "@base/database";
+import type { DataOrError } from "@base/dataOrError";
 import type { Chart } from "./chart";
 
-export interface Database<T extends Chart> extends BaseDatabase<T> {}
+export interface Database<T extends Chart> extends BaseDatabase<T> {
+    /**
+     * Get the image of the boss of this chart.
+     *
+     * @param chart A Ongeki chart.
+     * @returns Image buffer or error.
+     */
+    getBossCard(chart: T): Promise<DataOrError<Buffer>>;
+}
